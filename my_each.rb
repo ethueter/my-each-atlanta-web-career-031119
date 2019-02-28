7909cf447dc76cd7757ea78b48309903d90771e1
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-28 14:48:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,7 @@
-def my_each (array) {|name| 
+def my_each (array) {|name| "Hello, #{name}"}
   x = 0 
   while x < array.length do
-    yield(name)
-  "Hello, #{name}"
+    yield(array)
   x +=1 
 end 
 end
